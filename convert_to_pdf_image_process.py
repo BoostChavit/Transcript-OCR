7b1191a4f7691780871070370cc63860501de23a
BoostChavit <chavit.si@ku.th>
2025-03-06 13:45:52 +0700

    update : extract cell from cropped table

--- convert_to_pdf_image_process.py
+++ convert_to_pdf_image_process.py
@@ -3,6 +3,20 @@ import pytesseract
 import csv
 from matplotlib import pyplot as plt
 
+def fill_table(image, row_pos, columns_pos, table_data):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.5
+    thickness = 1
+
+    for row_idx, (y1, _) in enumerate(row_pos[:-1]):
+        for col_idx, (x1, _) in enumerate(columns_pos[:-1]):
+            position = (x1 + 5, y1 + 15)  # Adjust text position
+
+            # Put text in the image
+            cv2.putText(image, table_data, position, font, font_scale, (0, 0, 0), thickness)
+
+    return image
+
 def detect_tables(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, binary = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY_INV)
@@ -12,32 +26,32 @@ def detect_tables(image):
     vertical_lines = cv2.morphologyEx(binary, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
     table_mask = cv2.add(horizontal_lines, vertical_lines)
 
-    # get table by finding range in histogram
-    horizontal_hist = cv2.reduce(table_mask, 1, cv2.REDUCE_AVG).flatten()
-    horizontal_pos = []
+    # get each table by finding range in histogram
+    table_hist = cv2.reduce(table_mask, 1, cv2.REDUCE_AVG).flatten()
+    table_pos = []
     temp_pos = 0
-    for i, v in enumerate(horizontal_hist):
+    for i, v in enumerate(table_hist):
         if v > 0:
             if temp_pos == 0:
-                horizontal_pos.append((i, v))
+                table_pos.append((i, v))
             temp_pos = i
 
         if v == 0:
-            if horizontal_pos and horizontal_hist[i - 1] > 0:
-                temp_v = horizontal_hist[i - 1]
-                horizontal_pos.append((i -1, temp_v))
+            if table_pos and table_hist[i - 1] > 0:
+                temp_v = table_hist[i - 1]
+                table_pos.append((i -1, temp_v))
             temp_pos = 0
     
-    for i in range(len(horizontal_pos) - 1):
-        temp_image = binary[horizontal_pos[i][0]:horizontal_pos[i + 1][0], :]
 
-        height = (horizontal_pos[i + 1][0] - horizontal_pos[i][0])
+    for i in range(len(table_pos) - 1):
+        temp_image = binary[table_pos[i][0]:table_pos[i + 1][0], :]
+
+        height = (table_pos[i + 1][0] - table_pos[i][0])
         if height < 40:
             continue
 
-        vertical_lines = cv2.morphologyEx(temp_image, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
-
         # get columns by finding range in histogram
+        vertical_lines = cv2.morphologyEx(temp_image, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
         vertical_hist = cv2.reduce(vertical_lines, 0, cv2.REDUCE_AVG).flatten()
         threshold_gap = int(image.shape[1] * 0.01)
         columns_pos = []
@@ -61,41 +75,38 @@ def detect_tables(image):
             continue
 
         print(len(columns_pos))
-        gray_image = gray.copy()
-        # take out table grid lines
-        for yi in range(gray_image.shape[0]):
-            for xi in range(gray_image.shape[1]):
-                if table_mask[yi][xi]  > 40:
-                    gray_image[yi][xi] = 255
+
+        # find rows
+        horizontal_lines = cv2.morphologyEx(temp_image, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+        horizontal_hist = cv2.reduce(horizontal_lines, 1, cv2.REDUCE_AVG).flatten()
+        row_pos = []
+        temp_pos = 0
+        for k, v in enumerate(horizontal_hist):
+            if v > 0:
+                if temp_pos == 0:
+                    row_pos.append((k, v))
+                if k != 0:
+                    temp_pos = k
+                else:
+                    temp_pos = 1
+
+            if v == 0:
+                temp_pos = 0
+
+        for k in range(len(row_pos)):
+            temp_i, temp_v = row_pos[k]
+            cv2.circle(temp_image, (100, temp_i), 3, (255), 5)
         
-        temp_image = gray_image[horizontal_pos[i][0]:horizontal_pos[i + 1][0], :]
+        print(len(row_pos))
 
-        text = pytesseract.image_to_string(temp_image, lang="tha+eng", config="--psm 6")
-        print(text)
+        filling_text_image = gray[table_pos[i][0]:table_pos[i + 1][0], :]
+        filling_text_image = fill_table(filling_text_image, columns_pos, row_pos, "hi")
 
-        cv2.imshow('image', temp_image)
+        cv2.imshow('image', filling_text_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     # return table_bbox, table_mask
-
-def extract_from_table(image, table_bbox, table_mask, table_pos):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    x, y, w, h = table_bbox
-
-    # extract to text
-    text = ""
-    for i in range(len(table_pos) - 1):
-        cropped_table = gray_image[y:y+h, table_pos[i][0]:table_pos[i + 1][0]]
-
-        text += pytesseract.image_to_string(cropped_table, lang="eng", config="--psm 6")
-    return text
-
-def extract_from_image(image, table_bbox):
-    x, y, w, h = table_bbox
-    detail = image[:y, :x+w]
-    text = pytesseract.image_to_string(detail, lang="tha+eng", config="--psm 6")
-    return text
 
 image_path = 'page_1.png'
 image = cv2.imread(image_path)
--- main.py
+++ main.py
@@ -6,25 +6,25 @@ from pdf2image import convert_from_path
 from pytesseract import Output
 from collections import defaultdict
 
-# # Convert PDF to images (one image per page)
-# images = convert_from_path("transcript_sample/6310400908.pdf", dpi=300)
+# Convert PDF to images (one image per page)
+images = convert_from_path("CourseInspectionForm2560.pdf", dpi=300)
 
-# # Process each page
-# for i, image in enumerate(images):
-#     image_path = f"page_{i+1}.png"
-#     image.save(image_path, "PNG")
-#     print(f"Saved: {image_path}")
+# Process each page
+for i, image in enumerate(images):
+    image_path = f"page_{i+1}.png"
+    image.save(image_path, "PNG")
+    print(f"Saved: {image_path}")
 
-#     # Extract text using Tesseract (Thai + English)
-#     text = pytesseract.image_to_string(image, lang="tha+eng")
+    # Extract text using Tesseract (Thai + English)
+    text = pytesseract.image_to_string(image, lang="tha+eng")
     
-#     print(f"Text from page {i+1}:\n{text}\n{'-'*50}")
+    print(f"Text from page {i+1}:\n{text}\n{'-'*50}")
 
-# image = Image.open('page_1.png')
-image = cv2.imread('page_1.png')
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-_, binary_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+# # image = Image.open('page_1.png')
+# image = cv2.imread('page_1.png')
+# gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# _, binary_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-text = pytesseract.image_to_string(binary_image, lang="tha+eng")
-print(f"Text from page 1:\n{text}\n{'-'*50}")
+# text = pytesseract.image_to_string(binary_image, lang="tha+eng")
+# print(f"Text from page 1:\n{text}\n{'-'*50}")
  
